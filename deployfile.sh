8c2b7d75f58be04d921a3c726aac0d416e122e14
Nwachukwu Ugwuanyi <nwachukwu.ugwuanyi@gmail.com>
2017-12-23 23:52:13 +0100

    updated environment variables settings

--- deployfile.sh
+++ deployfile.sh
@@ -1,16 +1,22 @@
 if [[ $(which docker) && $(docker --version) ]]; then
     echo "docker is installed"
-        # variables defined in .env will be exported into this script's environment:
+	# variables defined in .env will be exported into this script's environment:
 	# Let's populate the variables in our compose file template,
 	set -a
 	. ./.env
 
 	# then deploy it!
+
 	cat docker-compose.yml | envsubst | docker-compose up --build
   else
     echo "Install docker"
 
 fi
+
+
+
+
+
 
 
 
